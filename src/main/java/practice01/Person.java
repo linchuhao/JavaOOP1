de1ea6bdf7599730473d249058602224d6230280
Lester Lin <LINLE5@OOCL.COM>
2020-07-22 21:10:39 +0800

    [test][pratice01] [Lester Lin] pratice01.

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,25 @@
 package practice01;
 
 public class Person {
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private String name;
+
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is "+ this.name + ". I am "+ this.age + " years old.";
+    }
 }
