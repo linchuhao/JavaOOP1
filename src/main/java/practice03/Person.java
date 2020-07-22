2286eb8055a2bec79f7a1e63cdc33ef6e6bbe292
Lester Lin <LINLE5@OOCL.COM>
2020-07-22 21:14:29 +0800

    [test][pratice01] [Lester Lin] Finish pratice03.

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,33 @@
 package practice03;
 
 public class Person {
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+
+    private  int age;
+
+    public String introduce(){
+        return "I am a"+ this.age + " years old.";
+    }
 }
