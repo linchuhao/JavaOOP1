446ce6b1f2acdeb514c5554791f51db482a51f8c
Lester Lin <LINLE5@OOCL.COM>
2020-07-22 21:17:01 +0800

    [test][pratice05] [Lester Lin] Finish pratice05.

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,39 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+
+    private String name;
+
+    private int age;
+
+    public Worker(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String introduce(){
+        return "My name is "+ this.name + ". I am " + this.age + " years old. I am a Worker. I have a job.";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
