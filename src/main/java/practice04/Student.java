60433ab4657c2fe4210d2b849ae94cf04ebe2c81
Lester Lin <LINLE5@OOCL.COM>
2020-07-22 21:15:19 +0800

    [test][pratice04] [Lester Lin] Finish pratice04.

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,20 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+
+    public int getKlass() {
+        return klass;
+    }
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    @Override
+    public String introduce(){
+        String str = super.introduce();
+        return str + " I am a Student. " + "I am at Class " + this.klass + ".";
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,39 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+
+    private int age;
+
+    public Worker(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String introduce(){
+        return "My name is "+ this.name + ". I am " + this.age + " years old. I am a Worker. I have a job.";
+    }
 }
